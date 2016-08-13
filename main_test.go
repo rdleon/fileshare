51a820055896181b7ef813d70c158e8ca023104a
R. Díaz de León <leon@elinter.net>
2016-08-13 01:18:39 -0500

    Added another test

--- main_test.go
+++ main_test.go
@@ -44,3 +44,38 @@ func TestInvalidHeaderLogin(t *testing.T) {
 	}
 
 }
+
+func TestMalformedJSONLogin(t *testing.T) {
+	var expected, response Response
+
+	expected = Response{"error": "Bad Request"}
+
+	r, _ := http.NewRequest("POST", "/login", strings.NewReader("TeST"))
+	r.Header.Set("Content-Type", "text/json")
+	w := httptest.NewRecorder()
+
+	LoginHandler(w, r)
+
+	if w.Code != http.StatusBadRequest {
+		t.Errorf("Expected status %d got %v", http.StatusBadRequest, w.Code)
+	}
+
+	b, err := ioutil.ReadAll(w.Body)
+	if err != nil {
+		t.Error("Can't read the response body")
+	}
+
+	err = json.Unmarshal(b, &response)
+	if err != nil {
+		t.Error("Can't read the response json")
+	}
+
+	for key, v := range expected {
+		if _, ok := response[key]; !ok {
+			t.Errorf("Expected %s:%v but key is missing in %v", key, v, len(response))
+		} else if response[key] != v {
+			t.Errorf("Expected %s:%v got %v", key, v, response[key])
+		}
+	}
+
+}
