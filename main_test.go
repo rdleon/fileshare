0b2c34241fc8d4e7a1643409a5eabaca3ae27e2b
R. Díaz de León <leon@elinter.net>
2016-08-13 00:48:59 -0500

    Added another simple test

--- main_test.go
+++ main_test.go
@@ -1,27 +1,33 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
+type Response map[string]interface{}
+
 func TestPostLogin(t *testing.T) {
 	var tests = []struct {
 		Body     string
-		Response map[string]interface{}
+		Response Response
 		Status   int
 	}{
 		{
 			"uahruhh.r'hrjhauaeua",
-			map[string]interface{}{
-				"error": "Not Found",
+			Response{
+				"error": "Must send content as text/json",
 			},
 			400,
 		},
 	}
 
 	for _, test := range tests {
+		var response Response
+
 		r, _ := http.NewRequest("POST", "/login", nil)
 		w := httptest.NewRecorder()
 
@@ -30,5 +36,17 @@ func TestPostLogin(t *testing.T) {
 		if w.Code != test.Status {
 			t.Errorf("Expected status %d got %v", test.Status, w.Code)
 		}
+
+		b, _ := ioutil.ReadAll(w.Body)
+		json.Unmarshal(b, &response)
+
+		for key, v := range test.Response {
+			if _, ok := response[key]; !ok {
+				t.Errorf("Expected %s:%v but key is missing in %v", key, v, len(response))
+			} else if response[key] != v {
+				t.Errorf("Expected %s:%v got %v", key, v, response[key])
+			}
+		}
+
 	}
 }
