39f70c68c8212a889547f3d80daea634807e125c
R. Díaz de León <leon@elinter.net>
2016-08-13 22:37:40 -0500

    Gave the login/logout routines their own home

--- main.go
+++ main.go
@@ -2,29 +2,15 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
-	"strings"
-	"time"
 
-	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
 )
 
-type User struct {
-	Name     string
-	Password string
-}
-
-var (
-	MyUser User
-	Conf   map[string]string
-)
+var Conf map[string]string
 
 func init() {
-	MyUser = User{Name: "rdleon", Password: "password"}
 	// TODO: Read config file
 	Conf = map[string]string{
 		"addr":      "127.0.0.1:8080",
@@ -52,96 +38,6 @@ func main() {
 	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./public/")))
 
 	log.Fatal(http.ListenAndServe(Conf["addr"], r))
-}
-
-// Validate the webtoken and return the username
-func CheckAuth(r *http.Request) (string, bool) {
-	tokenStr := r.Header.Get("Authorization")
-
-	if tokenStr == "" {
-		return "", false
-	}
-
-	i := strings.Index(tokenStr, "Bearer ")
-
-	if i >= 0 {
-		tokenStr = tokenStr[i+len("Bearer "):]
-	}
-
-	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method %v", token.Header["alg"])
-		}
-
-		return []byte(Conf["secretKey"]), nil
-	})
-
-	if err != nil {
-		return "", false
-	}
-
-	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		return claims["sub"].(string), true
-	}
-
-	return "", false
-}
-
-// Authenticates the user using name:password and generates a JWT
-func LoginHandler(w http.ResponseWriter, r *http.Request) {
-	var (
-		credentials User
-		response    map[string]interface{}
-	)
-
-	if _, ok := CheckAuth(r); ok {
-		fmt.Fprintf(w, "{\"loggedin\": true}")
-		return
-	}
-
-	if content := r.Header.Get("Content-Type"); content == "application/json" {
-
-		decoder := json.NewDecoder(r.Body)
-		err := decoder.Decode(&credentials)
-
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			fmt.Fprintf(w, "{\"error\": \"Bad Request\"}")
-			return
-		}
-
-		response = make(map[string]interface{})
-
-		if credentials == MyUser {
-			claims := jwt.StandardClaims{
-				ExpiresAt: time.Now().Add(time.Hour * 2).Unix(),
-				Issuer:    "fileshare",
-				Subject:   MyUser.Name,
-			}
-			token := jwt.NewWithClaims(jwt.SigningMethodHS256, &claims)
-
-			response["token"], err = token.SignedString([]byte(Conf["secretKey"]))
-
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				log.Println("Error: ", err)
-				fmt.Fprintf(w, "{\"error\": \"Internal Server Error\"}")
-				return
-			}
-		} else {
-			response["error"] = "Wrong username or password"
-		}
-
-		json.NewEncoder(w).Encode(response)
-	} else {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintf(w, "{\"error\": \"Must send content as application/json\"}")
-	}
-}
-
-// Logout the user by "destroying" the auth token before it expires
-func LogoutHandler(w http.ResponseWriter, r *http.Request) {
-	// TODO: destroy JWT token, expire session
 }
 
 // Given an string id searches for a file and delivers it to the client
