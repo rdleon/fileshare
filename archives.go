4323184ca202654ef0401f03cef4fe730c3434e7
R. Díaz de León <leon@elinter.net>
2016-08-17 12:48:31 -0500

    Added handling for the files

--- archives.go
+++ archives.go
@@ -3,16 +3,20 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/landjur/golibrary/uuid"
 )
 
 type Archive struct {
 	SavePath string
 	Name     string
 	Key      string
-	Expire   int
+	Expire   int64
 }
 
 var ArchiveStore map[string]Archive
@@ -20,9 +24,12 @@ var ArchiveStore map[string]Archive
 // Given an string id searches for a file and delivers it to the client
 func DownloadArchiveHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	if _, ok := vars["archiveKey"]; ok {
-		fmt.Fprintf(w, "{\"status\": \"Found\"}")
-		return
+	if key, ok := vars["archiveKey"]; ok {
+		archive := ArchiveStore[key]
+		if archive.Expire < time.Now().Unix() {
+			http.ServeFile(w, r, archive.SavePath)
+			return
+		}
 	}
 
 	w.WriteHeader(http.StatusNotFound)
@@ -39,6 +46,7 @@ func UpdateArchiveHandler(w http.ResponseWriter, r *http.Request) {
 
 	vars := mux.Vars(r)
 	if _, ok := vars["archiveKey"]; ok {
+		// TODO: Actually update the archive info
 		fmt.Fprintf(w, "{\"status\": \"updated\"}")
 		return
 	}
@@ -90,6 +98,58 @@ func AddArchiveHandler(w http.ResponseWriter, r *http.Request) {
 	if _, ok := CheckAuth(r); !ok {
 		w.WriteHeader(http.StatusUnauthorized)
 		fmt.Fprintf(w, "{\"error\": \"Unauthorized\"}")
+		return
+	}
+
+	r.ParseForm()
+	file, handler, err := r.FormFile("upload")
+	defer file.Close()
+	if err != nil {
+		// Bad Request
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "{\"error\": \"Bad Request\"}")
+		return
+	}
+
+	if handler != nil {
+		r.ParseMultipartForm(32 << 20)
+		// TODO: Read the save directory from Conf
+		path := "/tmp/" + handler.Filename
+		f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "{\"error\": \"Internal Server Error\"}")
+			return
+		}
+		defer f.Close()
+		io.Copy(f, file)
+		// TODO: Generate a cool key
+		key, err := uuid.NewV4()
+
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "{\"error\": \"Internal Server Error\"}")
+			return
+		}
+
+		keyStr := key.String()
+
+		ArchiveStore[keyStr] = Archive{
+			Key:      keyStr,
+			SavePath: path,
+			Name:     handler.Filename,
+			Expire:   time.Now().Add(time.Hour * 24).Unix(),
+		}
+
+		resp := map[string]string{
+			"key": keyStr,
+		}
+
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(resp)
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "{\"error\": \"Bad Request\"}")
 		return
 	}
 }
