e59d547fb5c184defeac86eb5600056a3f112c66
R. Díaz de León <leon@elinter.net>
2016-08-21 00:08:06 -0500

    Using http functionality to libutil.js

--- public/share.js
+++ public/share.js
@@ -1,88 +1,15 @@
-function isFunction(functionToCheck) {
-    var getType = {};
-    return functionToCheck && getType.toString.call(functionToCheck) === '[object Function]';
-}
-
-var http = {
-    jwt: null,
-    request: function (method, url, data, success, failure) {
-        var self = this;
-        var jsonStr = '';
-        var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
-
-        xhr.open(method, url, true)
-
-        xhr.onreadystatechange = function () {
-            if (isFunction(success) && xhr.readyState > 3 && xhr.status == 200) {
-                success(xhr.responseText);
-            } else if (isFunction(failure) && xhr.status >= 400) {
-                failure(xhr.status, xhr.responseText)
-            }
-        };
-
-
-        xhr.setRequestHeader('X-Request-With', 'XMLHttpRequest')
-        if (self.jwt) {
-            xhr.setRequestHeader('Authorization', 'Bearer ' + self.jwt);
-        }
-
-        if ((method == 'PUT' || method == 'POST') && data) {
-            jsonStr = JSON.stringify(data)
-            xhr.setRequestHeader('Content-Type', 'application/json')
-            xhr.send(jsonStr);
-        } else {
-            xhr.send();
-        }
-
-        return xhr;
-    },
-    get: function (url, success, failure) {
-        var self = this;
-        return self.request('GET', url, null, success, failure)
-    },
-    post: function (url, data, success, failure) {
-        var self = this;
-        return self.request('POST', url, data, success, failure)
-    },
-    put: function (url, data, success, failure) {
-        var self = this;
-        return self.request('POST', url, data, success, failure)
-    },
-    'delete': function (url, success, failure) {
-        var self = this;
-        return self.request('DELETE', url, null, success, failure)
-    },
-    uploadFile: function (url, file, success) {
-        var self = this;
-        var formData = new FormData();
-        var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
-
-        xhr.open('POST', url, true);
-        xhr.setRequestHeader('X-Request-With', 'XMLHttpRequest')
-
-        formData.append('upload', file, file.name);
-
-        if (self.jwt) {
-            xhr.setRequestHeader('Authorization', 'Bearer ' + self.jwt);
-        }
-
-        xhr.send(formData);
-    }
-};
-
-
-function onLogin(res) {
-    // Set the JWT
-    auth = JSON.parse(res);
-    if (auth.token) {
-        http.jwt = auth.token;
-        drawAdmin();
-    }
-}
-
 function drawMainInput() {
     var div = document.createElement('div'),
         input = document.createElement('input');
+
+    function onLogin(res) {
+        // Set the JWT
+        auth = JSON.parse(res);
+        if (auth.token) {
+            http.setJWT(auth.token);
+            drawAdmin();
+        }
+    }
 
     div.id = 'maincenter';
     div.class = 'simpleForm';
@@ -156,4 +83,5 @@ function drawAdmin() {
     document.body.appendChild(div);
 }
 
+var http = new HTTP();
 document.addEventListener('DOMContentLoaded', drawMainInput);
