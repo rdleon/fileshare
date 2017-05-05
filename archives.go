c6825637a0005dbce5705cc077dc546a1f120d57
R. Diaz de Leon <leon@elinter.net>
2017-05-05 17:57:39 -0500

    Downloading files correctly

--- archives.go
+++ archives.go
@@ -24,16 +24,31 @@ var ArchiveStore map[string]Archive
 // Given an string id searches for a file and delivers it to the client
 func DownloadArchiveHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	if key, ok := vars["archiveKey"]; ok {
-		archive := ArchiveStore[key]
-		if archive.Expire < time.Now().Unix() {
-			http.ServeFile(w, r, archive.SavePath)
-			return
-		}
+	key, ok := vars["archiveKey"]
+
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "{\"error\": \"Not found\"}")
+		return
 	}
 
-	w.WriteHeader(http.StatusNotFound)
-	fmt.Fprintf(w, "{\"error\": \"Not found\"}")
+	archive, ok := ArchiveStore[key]
+
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "{\"error\": \"Not found\"}")
+		return
+	}
+
+	if archive.Expire < time.Now().Unix() {
+		// The archive have expire, clean it up!
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "{\"error\": \"Not found\"}")
+		return
+	}
+
+	w.Header().Set("Content-Disposition", "attachment; filename="+archive.Name)
+	http.ServeFile(w, r, archive.SavePath)
 }
 
 // Updates the file name and expire date
@@ -124,7 +139,7 @@ func AddArchiveHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		defer f.Close()
 		io.Copy(f, file)
-		// TODO: Generate a cool key
+		// TODO: Generate a ~COOL~ key
 		key, err := uuid.NewV4()
 
 		if err != nil {
